9f446417f13f712b6bdd876e2e15e04623de521d
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-03-28 09:40:49 -0300

    att

--- src/styles.js
+++ src/styles.js
@@ -14,6 +14,7 @@ export const Container = styled.div`
     align-items: center;
     gap: 44px;
 
+    height:100vh;
 `;
 
 export const Image = styled.img`
@@ -33,7 +34,7 @@ export const LayoutCel = styled.div`
     background: linear-gradient(157.44deg, rgba(255, 255, 255, 0.6) 0.84%, rgba(255, 255, 255, 0.6) 0.85%, rgba(255, 255, 255, 0.15) 100%);
     border-radius: 61px 61px 0px 0px;
     
-
+    height: 100vh;
 `;
 
 
@@ -88,9 +89,70 @@ color: #FFFFFF;
 margin-bottom: 34px;
 `;
 
- export const Button = styled.button`
+export const Button = styled.button`
 
-  
+background: rgba(0, 0, 0, 0.8);
+border-radius: 14px;
 
+
+width: 342px;
+height: 74px;
+color: white;
+font-style: normal;
+font-weight: 700;
+font-size: 17px;
+line-height: 28px;
+
+border: none;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+gap: 30px;
+
+cursor: pointer;
+
+&:hover {
+  opacity:0.8;
+}
+
+&:active {
+  opacity:0.5;
+}
+
+`;
+
+
+export const User = styled.li`
+
+display: flex;
+justify-content: space-around;
+align-items: center;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+width: 342px;
+height: 58px;
+border: none;
+outline: none;
+margin-top: 20px;
+
+
+p{
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 28px;
+
+color: #FFFFFF;
+}
+
+button{
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
 
 `;
