4a5816de5427a2b9c217be4773bde29d3ca8148c
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-04-07 10:29:13 -0300

    Salvando alteração

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './page/Home';
-/*  import Users from './page/Users'; */
+import Users from './page/Users'; 
 import GlobalStyle from './globalStyle';
 
 
 ReactDOM.render(
 
  <>
-   <Home/> 
+   <Users/> 
    <GlobalStyle/>
  </>,
 
